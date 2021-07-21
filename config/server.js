4de15e4bb6654728af4b84ce986a9f6a072055b4
Alex <1080810@alumno.um.edu.mx>
2021-07-21 17:52:47 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f345cf9965b88d94925f8897a978c29a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e33bc23a-50e8-4ecb-a2dc-7fdfaa9016b6'
+};
