41cbe944e0dabc3ca272dbfa3937e5e0a609ae14
Alex <1080810@alumno.um.edu.mx>
2021-09-09 11:02:23 -0500

    Add custom endpoints for banners

--- api/banners/controllers/banners.js
+++ api/banners/controllers/banners.js
@@ -1,8 +1,35 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    list: async ctx => {
+        // const result = await strapi
+        //     .query('Banners')
+        //     .model.fetchAll({ columns: ['id', 'title', 'startDate', 'endDate'] })
+        // const fields = result.toJSON();
+
+        // ctx.send(fields);
+        let entities;
+        entities = await strapi.services.banners.find("");
+
+        return entities.map(entity => {
+            const banner = sanitizeEntity(entity, {
+                model: strapi.models.banners,
+            });
+
+            delete banner.items;
+            delete banner.published_at;
+            delete banner.created_at;
+            delete banner.updated_at;
+            delete banner.cost;
+            delete banner.description;
+
+            return banner;
+        });
+    },
+};
